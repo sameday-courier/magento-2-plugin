0d3800080887e46631867cd79ddf54c7bd9e9999
Daniel Gorgan <danut007ro@gmail.com>
2019-03-06 17:51:41 +0200

    test

---
+++ Shipping/Controller/Adminhtml/Service/Edit.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace SamedayCourier\Shipping\Controller\Adminhtml\Service;
+
+use Magento\Framework\Controller\ResultFactory;
+
+class Edit extends \Magento\Backend\App\Action
+{
+    /**
+     * @return \Magento\Backend\Model\View\Result\Page
+     */
+    public function execute()
+    {
+        /** @var \Magento\Backend\Model\View\Result\Page $resultPage */
+        $resultPage = $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+
+        return $resultPage;
+    }
+}
