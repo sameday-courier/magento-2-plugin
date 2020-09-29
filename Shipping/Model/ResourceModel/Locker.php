fb3ebeb2ca240c4a315e3fec12e6e4e4e77377fe
Dumitru Alexandru <alexandru.dumitru1@sameday.ro>
2020-09-29 16:37:30 +0300

    Update missing service name

--- Shipping/Model/ResourceModel/Locker.php
+++ Shipping/Model/ResourceModel/Locker.php
@@ -3,7 +3,6 @@
 namespace SamedayCourier\Shipping\Model\ResourceModel;
 
 use Magento\Framework\Model\ResourceModel\Db\AbstractDb;
-use SamedayCourier\Shipping\Api\Data\ServiceInterface;
 
 class Locker extends AbstractDb
 {
