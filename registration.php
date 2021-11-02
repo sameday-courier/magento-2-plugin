732174b317e70aec14784ebce9adc29139c324a8
Michael Mussulis <michael@xtreme-vision.net>
2021-11-02 08:24:54 -0400

    Fixed module path in registration.php

--- registration.php
+++ registration.php
@@ -1,6 +1,7 @@
 <?php
 
 \Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE, 'SamedayCourier_Shipping',
-    isset($file) ? dirname($file) : __DIR__
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'SamedayCourier_Shipping',
+    __DIR__
 );
