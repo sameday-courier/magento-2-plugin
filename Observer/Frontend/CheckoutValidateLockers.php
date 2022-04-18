2768de5dbac4aa65af1e1f18aa26b7d3a6c8322b
Alexandru Soptareanu <alexandru.soptareanu@sameday.ro>
2022-04-18 22:38:17 +0300

    Show locker details on checkout page.

--- Observer/Frontend/CheckoutValidateLockers.php
+++ Observer/Frontend/CheckoutValidateLockers.php
@@ -7,22 +7,32 @@ namespace SamedayCourier\Shipping\Observer\Frontend;
 use Magento\Framework\App\RequestInterface;
 use Magento\Framework\Event\ObserverInterface;
 use Magento\Framework\Event\Observer as EventObserver;
+use Magento\Framework\Serialize\Serializer\Json;
 
 class CheckoutValidateLockers implements ObserverInterface
 {
+    /**
+     * @var RequestInterface $request
+     */
     protected $request;
 
-    public function __construct(RequestInterface $request)
+    /**
+     * @var Json $json
+     */
+    protected $json;
+
+    public function __construct(RequestInterface $request, Json $json)
     {
         $this->request = $request;
+        $this->json = $json;
     }
 
     public function execute(EventObserver $observer)
     {
         $order = $observer->getOrder();
-        $samedayLockerCookie = (int) $this->request->getCookie('samedaycourier_locker_id', null);
-        if ($samedayLockerCookie) {
-            $order->setData('samedaycourier_locker', $samedayLockerCookie);
+        $samedayLocker = $this->json->unserialize($this->request->getCookie('samedaycourier_locker', null));
+        if (null !== $samedayLocker && $order->getData()['shipping_method'] === 'samedaycourier_15') {
+            $order->setData('samedaycourier_locker', $samedayLocker['lockerId']);
         }
     }
 }
