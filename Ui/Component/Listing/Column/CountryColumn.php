f107260d35a5a67c98b16e74ecf2c7ebb17663f3
Alexandru Soptareanu <alexandru.soptareanu@sameday.ro>
2024-11-19 14:37:53 +0200

    SIP-718. WIP.

--- Ui/Component/Listing/Column/CountryColumn.php
+++ Ui/Component/Listing/Column/CountryColumn.php
@@ -3,19 +3,36 @@
 namespace SamedayCourier\Shipping\Ui\Component\Listing\Column;
 
 use Magento\Framework\Data\OptionSourceInterface;
+use Magento\Framework\View\Element\UiComponent\ContextInterface;
+use Magento\Framework\View\Element\UiComponentFactory;
 use Magento\Ui\Component\Listing\Columns\Column;
+use SamedayCourier\Shipping\Helper\ApiHelper;
 
 class CountryColumn extends Column implements OptionSourceInterface
 {
+    /**
+     * @var ApiHelper $apiHelper
+     */
+    protected $apiHelper;
+
+    public function __construct(
+        ContextInterface $context,
+        UiComponentFactory $uiComponentFactory,
+        ApiHelper $apiHelper,
+        array $components = [],
+        array $data = []
+    )
+    {
+        parent::__construct($context, $uiComponentFactory, $components, $data);
+
+        $this->apiHelper = $apiHelper;
+    }
+
     /**
      * @return array[]
      */
     public function toOptionArray(): array
     {
-        return [
-            ['value' => 187, 'label' => 'Romania'],
-//            ['value' => 34, 'label' => 'Bulgaria'],
-//            ['value' => 237, 'label' => 'Hungary'],
-        ];
+        return [$this->apiHelper::DEFAULT_COUNTRIES[$this->apiHelper->getHostCountry()]];
     }
 }
